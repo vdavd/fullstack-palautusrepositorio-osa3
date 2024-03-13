77d58840c0987ffa14ddc326d54086a6c8f67e8d
Davidsson Ville J <davidavi@cubbli22-gold.local>
2024-03-13 17:10:58 +0200

    uibuild

--- puhelinluettelon_backend/index.js
+++ puhelinluettelon_backend/index.js
@@ -6,6 +6,7 @@ const app = express()
 
 morgan.token('body', (req, res) => {return JSON.stringify(res.req.body)})
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
