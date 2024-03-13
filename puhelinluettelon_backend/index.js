d64ec83f447d37df3a8a2db045a4b7a76c3a23f6
Davidsson Ville J <davidavi@cubbli22-gold.local>
2024-03-13 15:47:58 +0200

    added cors and made some changes to delete request

--- puhelinluettelon_backend/index.js
+++ puhelinluettelon_backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const app = express()
 var morgan =require('morgan')
+const cors = require('cors')
+
+const app = express()
 
 morgan.token('body', (req, res) => {return JSON.stringify(res.req.body)})
+
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -47,9 +51,14 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
     persons = persons.filter(person => person.id != id)
 
-    response.status(204).end()
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(204).end()
+    }
 })
 
 app.get('/info', (request, response) => {
@@ -64,7 +73,6 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(response.req.body)
 
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -89,7 +97,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
